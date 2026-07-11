9e128fc5189797570f0aa6b1c475716af5ab7d10
Sunita Chaulagain <sunitachaulagain300@gmail.com>
2026-07-11 22:32:12 +0545

    learned and revised http request, response and API endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,3 +5,19 @@ app = FastAPI()
 @app.get("/")
 def home():
     return{"message" : "Welcome to AI study Assistant API"}
+
+
+@app.get("/about")
+def about():
+    return {
+        "Project" : "AI Study Assistant",
+        "Version" : "1.0.0",
+        "developer" : "Sunita"
+
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status" : "server is running successfully!"
+    }
